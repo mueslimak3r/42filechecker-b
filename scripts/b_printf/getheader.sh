24c894e72846ad3c060e4c1aefca04a7f3e12f28
Cameron Lambert <calamber@e1z3r6p22.42.us.org>
2018-10-11 15:23:10 -0700

    fix

--- scripts/b_printf/getheader.sh
+++ scripts/b_printf/getheader.sh
@@ -4,7 +4,7 @@ red='\033[1;31m'
 nc='\033[0m'
 green='\033[4;32m'
 cd "$1"
-echo "${red}please enter the location/name of your .h file ${green}in the project drectory${nc}, then press enter."
+echo "${red}please enter the location/name of your .h file ${green}relative to root drectory of project${nc}, then press enter."
 echo "example: includes/b_printf.h"
 read hlocation
 
