4abaccb186d9317af8744d3d501706340113aa39
Cameron <cameronlambert98@gmail.com>
2018-10-11 06:34:18 -0700

    fix

--- run.sh
+++ run.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-git clean -f -d
+git clean -dfx
 git pull
+
 case $1 in
     "b_libft" ) sh scripts/b_libft/b_libft.sh "$2";;
     "b_printf" ) sh scripts/b_printf/b_printf.sh "$2";;
     "b_ls" ) sh scripts/b_ls/b_ls.sh "$2";;
-    "update" ) git reset --hard origin/master
-    *) echo "Invalid. Use ./run [projName] [projPath]";;
+    "update" ) git reset --hard origin/master;;
+    "cleanup" )
+        if [ -e "$2" ] ; then
+            cd "$2" && rm -rf logs
+            make fclean
+        fi;;
+    * ) echo "Invalid. Use ./run [projName] [projPath]";;
 esac
