db51582822e2c9e1cd3616bd05b6fb263c0fce40
Cameron Lambert <calamber@e1z3r6p22.42.us.org>
2018-10-11 16:48:55 -0700

    fix

--- scripts/norme.sh
+++ scripts/norme.sh
@@ -4,7 +4,7 @@ nc='\033[0m'
 green='\033[01;32m'
 cd $1
 rm -rf refLog yourLog logs refProg.dSYM yourProg.dSYM
-if [[ $(command -v norminette | grep -e) == "0" ]]
+if [[ $(command -v norminette | grep -e '') == "0" ]]
 then
     echo "${red}No norminette :'(${nc}"
 else
