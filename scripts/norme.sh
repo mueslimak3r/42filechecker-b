f6b38f0da110dc2a751455f11a948cc3fe55262d
Cameron Lambert <calamber@e1z3r6p22.42.us.org>
2018-10-11 16:44:07 -0700

    fix

--- scripts/norme.sh
+++ scripts/norme.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+red='\033[1;31m'
+nc='\033[0m'
+green='\033[01;32m'
 cd $1
 rm -rf refLog yourLog logs refProg.dSYM yourProg.dSYM
-norminette -R CheckForbiddenSourceHeader
+if [[ $(command -v norminette | grep -e) == "0" ]]
+then
+    echo "${red}No norminette :'(${nc}"
+else
+    norminette -R CheckForbiddenSourceHeader
