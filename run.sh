97456b8a3c0ea19d2e16d4197dbb0065fccf9001
Cameron <cameronlambert98@gmail.com>
2018-10-11 05:00:58 -0700

    change to auto-update

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-git fetch --all >/dev/null
+git clean -f -d
+git pull
 case $1 in
     "b_libft" ) sh scripts/b_libft/b_libft.sh "$2";;
     "b_printf" ) sh scripts/b_printf/b_printf.sh "$2";;
     "b_ls" ) sh scripts/b_ls/b_ls.sh "$2";;
+    "update" ) git reset --hard origin/master
     *) echo "Invalid. Use ./run [projName] [projPath]";;
 esac
