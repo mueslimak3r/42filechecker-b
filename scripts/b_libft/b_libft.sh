63289d8b595937ffe9a6bd698c8b66050b1f89eb
Cameron <cameronlambert98@gmail.com>
2018-10-11 07:28:17 -0700

    fix

--- scripts/b_libft/b_libft.sh
+++ scripts/b_libft/b_libft.sh
@@ -55,7 +55,7 @@ fi
 
 ./refProg | cat -e > refLog
 DIFF=$(diff yourLog refLog)
-if [ "$DIFF" == "" ] ; then
+if "$DIFF" == "" ; then
     echo "${green}pass!${nc}"
 else
     echo "${red}Outputs don't match! Check log files in project directory${nc}"
