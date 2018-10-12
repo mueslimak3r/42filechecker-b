f8744b4a8b073a6c8a05ec9a4ea74df640688560
Cameron <cameronlambert98@gmail.com>
2018-10-11 20:14:43 -0700

    fixed norm display

--- scripts/tools/norme.sh
+++ scripts/tools/norme.sh
@@ -5,7 +5,7 @@ green='\033[01;32m'
 yellow='\033[0;33m'
 cd $1
 rm -rf refLog yourLog logs refProg.dSYM yourProg.dSYM
-if [[ $(command -v norminette | grep -e "") == "0" ]]
+if [ $(command -v norminette | grep -e "") == "0" ]
 then
     echo "${red}No norminette :'(${nc}"
 else
