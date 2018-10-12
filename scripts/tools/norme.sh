a64fe527856488a2d37599e74f4ca6f51b9f29d8
Cameron Lambert <calamber@e1z3r6p15.42.us.org>
2018-10-11 22:03:21 -0700

    fix for linux

--- scripts/tools/norme.sh
+++ scripts/tools/norme.sh
@@ -6,13 +6,13 @@ yellow='\033[0;33m'
 cd $1
 rm -rf refLog yourLog logs refProg.dSYM yourProg.dSYM
 if [[ $(command -v norminette | grep -e "") == "" ]] ; then
-    echo "${red}No norminette :'(${nc}"
+    echo -e "${red}No norminette :'(${nc}"
 else
     norminette > norm_save
     warnings=$(grep -c "^Warning" norm_save)
     errs=$(grep -c "^Error" norm_save)
     cat norm_save
-    echo && echo "You have $warnings ${yellow}warning(s)${nc}"
-    echo "You have $errs ${red}error(s)${nc}"
+    echo && echo -e "You have $warnings ${yellow}warning(s)${nc}"
+    echo -e "You have $errs ${red}error(s)${nc}"
     rm -f norm_save
 fi
